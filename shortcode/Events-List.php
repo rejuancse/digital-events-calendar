0008738519e6669d6e5904d704243160ad75cc6d
Rejuan Ahamed <rejuancse@gmail.com>
2021-04-10 22:19:34 +0600

    Events list script update

--- shortcode/Events-List.php
+++ shortcode/Events-List.php
@@ -10,12 +10,44 @@ class Event_Listing {
     }
 
     function event_list_callback( $atts, $shortcode ){  
+
+        $atts = shortcode_atts( array(
+            'number' => '9',
+            'column' => '4',
+        ), $atts );
+
+        $args = array(
+            'post_type'      => 'event',
+            'posts_per_page' => $atts['number'],
+        );
+
+        $query = new \WP_Query($args);
+
         $output = '';
-	    $output .=' <div class="container">';
-	        require_once DEC_DIR_PATH . 'lib/events-listing.php';
-	        $Events_Listing_Shortcode = new \DEC\lib\Events_Listing_Shortcode();
-    		$output .= $Events_Listing_Shortcode->events_show();                
-	    $output .= '</div>';
+        $output .=' <div class="events-listing">';
+    	    $output .=' <div class="container">';                
+                $output .= '<div class="row">';
+                    while ( $query->have_posts() ) {
+                        $query->the_post();
+                        
+                        $output .= '<div class="col-md-'.$atts['column'].'">';
+                            $output .= '<div class="single-event-listing">';
+                                if ( has_post_thumbnail() ){
+                                    $output .= '<a href="'.get_the_permalink().'">';
+                                        $output .= get_the_post_thumbnail(get_the_ID(), array(300,400), array('class' => 'img-responsive'));
+                                    $output .= '</a>';
+                                }
+                                $output .= '<div class="title-wrap">';
+                                    $output .= '<h2><a href="'.get_the_permalink().'">'.get_the_title().'</a></h2>';
+                                    $output .= '<p>'.DEC()->limit_word_text(strip_tags(get_the_content()), 130).'</p>';
+                                $output .= '</div>';
+                            $output .= '</div>';
+                        $output .= '</div>';
+                    }
+                $output .='</div>';
+                                
+    	    $output .= '</div>';
+        $output .= '</div>';
 
 		return $output;
     }
